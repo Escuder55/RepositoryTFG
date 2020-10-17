d839208b87d1082c19ec136ebca39b219632d828
Martí <magopru1995@gmail.com>
2020-10-17 13:59:57 +0200

    LockCamera

---
+++ Game_TFG/Assets/Scripts/Camera/PlayerHandlerCamera.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerHandlerCamera : MonoBehaviour
+{
+    //REFERENCIA AL MODELO
+    public Transform model;
+
+    //target object
+    public Transform targetLock;
+
+    //VELOCIDAD DE ROTACION
+    [Range(20, 80f)]
+    public float rotationSpeed = 20f;
+
+    //CAMERA PRINCIPAL
+    private Camera mainCamera;
+
+    //REFERENCIA AL ANIMADOR
+    private Animator anim;
+
+    //INPUT DIRECTION
+    private Vector3 StickDirection;
+
+    private bool isWeaponEquipped = false;
+    private bool isTargetLocked = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        anim = model.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //WASD INPUT
+        StickDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        HandleInputData();
+
+        //HANDLE ROTATION PARA MIRAR A DONDE SEÑALAMOS
+        if(isTargetLocked)
+        {
+            HandleTargetLockedLocomotionRotation();
+        }
+        else
+        {
+            HandleStandardLocomotionRotation();
+        }
+    }
+
+    private void HandleStandardLocomotionRotation()
+    {
+        Vector3 rotationOffset = mainCamera.transform.TransformDirection(StickDirection);
+        rotationOffset.y = 0; //solo queremos los movimientos laterales.
+        //smooth rotation
+        model.forward += Vector3.Lerp(model.forward, rotationOffset, Time.deltaTime * rotationSpeed);
+    }
+
+    private void HandleTargetLockedLocomotionRotation()
+    {
+        //aqui referenciamos la rotacion del enemigo
+        //targetposition - currentposition = vector direction from currentposition to target position
+        Vector3 rotationOffset = targetLock.transform.position - model.position;
+        rotationOffset.y = 0; 
+        //smooth rotation
+        model.forward += Vector3.Lerp(model.forward, rotationOffset, Time.deltaTime * rotationSpeed);
+    }
+
+    private void HandleInputData()
+    {
+        //USING KEYBOARD AND PRESSING A DIAGONAL DIRECTION YOU COULD REACH A MAGNITUDE OF 1.4 so we clamp it.
+        anim.SetFloat("Speed", Vector3.ClampMagnitude(StickDirection, 1).magnitude);
+        anim.SetFloat("Horizontal", StickDirection.x);
+        anim.SetFloat("Vertical", StickDirection.z);
+        isWeaponEquipped = anim.GetBool("IsWeaponEquipped");
+        isTargetLocked = anim.GetBool("IsTargetLocked");
+
+        //SOLO PODEMOS TARGETEAR EL ENEMIGO CUANDO ESTA EL ARMA EQUIPADA
+        if(isWeaponEquipped && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("IsTargetLocked", !isTargetLocked);
+            isTargetLocked = !isTargetLocked;
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            //on y off
+            anim.SetBool("IsWeaponEquipped", !isWeaponEquipped);
+            //variable puede ser que la usemos despues
+            isWeaponEquipped = !isWeaponEquipped;
+            //si desequipamos el arma se nos sale del target lock state
+            if(isWeaponEquipped == false)
+            {
+                anim.SetBool("IsTargetLocked", false);
+                isTargetLocked = false;
+            }
+        }
+    }
+}
