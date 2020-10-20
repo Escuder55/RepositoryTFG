336718203a3a6982f8c7c2e1d70d8273e6aabd6a
Sergio E <Sergio.escuder5@gmail.com>
2020-10-20 15:52:51 +0200

    Item Behaviour Started

---
+++ Game_TFG/Assets/Item.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Item : MonoBehaviour
+{
+    #region VARIABLES
+    public enum ItemType
+    {
+        NONE,
+        CRYSTAL_DUST,
+        CRYSTAL,
+        LIFE_POTION,
+        ATTACK_POTION,
+        DEFENSE_POTION,
+        SPEED_POTION
+
+    };
+
+    public enum ItemBehaviour
+    {
+        NONE,
+        PICK_UP,
+        ATTRACK
+    };
+
+    [Header("ITEM TYPE")]
+    public ItemType item;
+    public ItemBehaviour behaviour;
+
+    [Header("ATTRACK")]
+    public float timeToAttrack;
+    public float speedAttractionItem;
+    Animator animator;
+    float timer;
+
+    Transform playerTransform;
+
+    #endregion
+
+    #region START
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        if (behaviour == ItemBehaviour.ATTRACK)
+        {
+            playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        }
+    }
+    #endregion
+
+    #region UPDATE
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= timeToAttrack)
+        {
+            animator.SetBool("Idle", true);
+
+            timer = 0;
+            Debug.Log("IR PARA EL JUGADOR");
+            this.transform.DOMove(new Vector3(  playerTransform.position.x, 
+                                                playerTransform.position.y, 
+                                                playerTransform.position.z), 
+                                                speedAttractionItem);
+        }
+    }
+    #endregion
+}
