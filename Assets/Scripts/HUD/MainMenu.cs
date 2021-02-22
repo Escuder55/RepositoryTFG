c57d6687d6276dd6b2b6c8542da1a5afcfedbfa8
Martí <magopru1995@gmail.com>
2021-02-22 11:19:41 +0100

    Settings Started

--- Assets/Scripts/HUD/MainMenu.cs
+++ Assets/Scripts/HUD/MainMenu.cs
@@ -14,8 +14,6 @@ public class MainMenu : MonoBehaviour
         //HAY QUE AÑADIR LA ESCENA AL BUILDEAR
         Debug.Log("Empecemos");
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        
-        Invoke("LoadNextScene", 0.5f);
     }
 
     public void ExitGame()
